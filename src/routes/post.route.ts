7f0572ac544d5fb890b76622e8b9300966e42888
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2023-08-27 10:06:36 +0100

    feature(update-post): update post

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import PostController from "../controllers/post.controller";
 import validate, { validator } from "../validation/validator";
-import { createPostSchema, IdSchema } from "../validation/schemas";
+import { createPostSchema, IdSchema, updatePostSchema } from "../validation/schemas";
 
-const { create, index, show } = PostController;
+const { create, index, show, update } = PostController;
 
 const router = express.Router();
 
 router.post("/", validator(createPostSchema), create);
 router.get("/", index);
 router.get("/:id", validator(IdSchema, "params"), show);
+router.patch('/:id', validator(IdSchema,  "params"), validator(updatePostSchema, 'body'), update )
 
 export default router;
