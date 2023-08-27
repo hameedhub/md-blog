93b32dda37abb00a6a5fa1e835c6ca0106beacb9
Hameed Abdulrahaman <hameedabdulrahamann@gmail.com>
2023-08-27 22:54:40 +0100

    deployment bug fix

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import app from "./index";
+// import app from "./index";
 
-const PORT: number = parseInt(process.env["PORT"] ?? "3000", 10);
+// const PORT: number = parseInt(process.env["PORT"] ?? "3000", 10);
 
-app.listen(PORT, () => {
-  console.log(`Listening on part ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Listening on part ${PORT}`);
+// });
